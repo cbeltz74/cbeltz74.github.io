08c1b27a258e33116587e6a2593b625d4ccac3b6
cbeltz74 <colinbeltz@gmail.com>
2020-04-19 10:50:21 -0500

    doggo done in classroom
    doggo done in classroom before finishing it on our own.

--- DogApp/JS/doggos.js
+++ DogApp/JS/doggos.js
@@ -1,16 +1,81 @@
 const BREEDS_URL = "https://dog.ceo/api/breeds/image/random";
 
-    fetch(BREEDS_URL)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            const img = document.createElement('img');
-            img.src = data.message;
-            img.alt = 'Cute doggo';
+function addDoggo () {
 
-            document.querySelector('.doggos').append(img);
-        })
+    // start loading spinner
+
+    fetch(BREEDS_URL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        const img = document.createElement('img');
+        img.src = data.message;
+        img.alt = 'Cute doggo';
+
+        document.querySelector('.doggos').append(img);
+
+        // stop loading spinner
+    })
+    
+}
+
+document.querySelector('.add-doggo').addEventListener
+("click", addDoggo);
+
+
+const BREEDS_LIST_URL = "https://dog.ceo/api/breeds/list/all";
+
+const select = document.querySelector('.breeds');
+
+
+
+fetch(BREEDS_LIST_URL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        const breedsObject = data.message;
+        const breedsArray = Object.keys(breedsObject);
+
+
+        for (let i = 0; i < breedsArray.length; i++) {
+            const option = document.createElement('option');
+            option.value = breedsArray[i];
+            option.innerHTML = breedsArray[i];
+            select.appendChild(option);
+        }
+    })
+
+    select.addEventListener("change", function(event) {
+        console.log(event.target.value);
+        console.log(`https://dog.ceo/api/breed/${event.target.value}/images/random`)
+    });
+
+let url = 'some url';
+
+// getNewDoggo(url);
+
+// make url
+
+// show loading spinner
+
+// fetch from the API
+
+// Use the url to change the current image
+
+// stop showing loading spinner
+
+
+
+
+
+
+
+
+
+
+
 
 
 
