b87b3a5fb30f475a6146db15d9a9e7a7f8507f7f
cbeltz74 <colinbeltz@gmail.com>
2020-04-19 08:14:01 -0500

    Started new gallery using Swiper.js
    Created new gallery using Swiper.js.
    Created new html, css and js files and copes photo folder from first version of photogallery.

---
+++ Gallery2/JS/gallery.js
@@ -0,0 +1,8 @@
+new Swiper(".swiper-containter", {
+    speed: 400,
+    space-between: 100,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
