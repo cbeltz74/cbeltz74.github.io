ef5cc537495362ca94cfb445c50ee1ec4b6c1c3c
cbeltz74 <colinbeltz@gmail.com>
2020-10-18 13:26:56 -0500

    Vid7 Added GlobalStyles.js file. import in Layout

--- master-gatsby-master/starter-files/gatsby/src/components/Layout.js
+++ master-gatsby-master/starter-files/gatsby/src/components/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Footer from './Footer';
 import Nav from './Nav';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Nav />
-      <p>I am the page content</p>
+      {children}
       <Footer />
     </div>
   );
