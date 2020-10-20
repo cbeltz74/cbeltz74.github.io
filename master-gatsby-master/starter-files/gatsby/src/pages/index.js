fd7df8e62049318261cc78df51dc4706d7895476
cbeltz74 <colinbeltz@gmail.com>
2020-10-19 20:44:18 -0500

    vid10 stylign layout

--- master-gatsby-master/starter-files/gatsby/src/pages/index.js
+++ master-gatsby-master/starter-files/gatsby/src/pages/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import Footer from '../components/Footer';
 import Layout from '../components/Layout';
-import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
     <Layout>
-      <Nav />
       <p>Hey! I'm the home page!</p>
-      <Footer />
     </Layout>
   );
 }
