a43bbd7be949ea5ff28577d557815aa713f2171e
cbeltz74 <colinbeltz@gmail.com>
2020-10-19 20:55:07 -0500

    vid10.2 corrected stripes border, removed 2x nav

--- master-gatsby-master/starter-files/gatsby/src/components/Layout.js
+++ master-gatsby-master/starter-files/gatsby/src/components/Layout.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Footer from './Footer';
-import Nav from './Nav';
+import styled from 'styled-components';
 import 'normalize.css';
+import Nav from './Nav';
+import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
-import styled from 'styled-components';
 import stripes from '../assets/images/stripes.svg';
 
 const SiteBorderStyles = styled.div`
   max-width: 1000px;
   margin: 12rem auto 4rem auto;
   margin-top: clamp(2rem, 10vw, 12rem);
-  background: red;
+  background: white url(${stripes});
   background-size: 80em;
   padding: 5px;
   padding: clamp(5px, 1vw, 25px);
