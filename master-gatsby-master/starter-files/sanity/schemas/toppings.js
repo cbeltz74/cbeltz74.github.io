54cb1292829e4476c84ab2f9bdb5eebb1e04472c
cbeltz74 <colinbeltz@gmail.com>
2021-01-04 19:07:07 -0600

    added topping schema

---
+++ master-gatsby-master/starter-files/sanity/schemas/toppings.js
@@ -0,0 +1,18 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // computer name
+  name: 'topping',
+  // visible title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+  ],
+};
